c3d16f6aeb83b4e2c0155648ac3ad77dc63f1e74
Zsombor <szilagyizs@kkszki.hu>
2025-09-11 13:05:49 +0200

    Kezdeti állapot

--- Menu/Program.cs
+++ Menu/Program.cs
@@ -10,7 +10,41 @@ namespace Menu
     {
         static void Main(string[] args)
         {
+            string name = "";
+            bool ffi = true;
+            int age = 0;
+            string choice = "";
 
+            do
+            {
+                Console.Clear();
+                Console.Title = "Személyesadatok kezelése";
+                Console.WriteLine("1 - Személyes adatok bevitele");
+                Console.WriteLine("2 - Személyes adatok kiíratása");
+                Console.WriteLine("3 - Kilépés");
+                Console.Write("\nVálasszon a menüből: ");
+                choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    Console.Clear();
+                    
+                    Console.WriteLine("\nSikeresen rögzítettük. Enterrel tovább...");
+                    Console.ReadLine();
+                }
+                else if (choice == "2")
+                {
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Rossz gomb, próbáld újra");
+                }
+
+
+
+            }
+            while (choice != "3");
         }
     }
 }
